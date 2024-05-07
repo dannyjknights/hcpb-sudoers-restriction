e7dcbbf50b6e643c8dece5015b5ba96ce21ab936
Amar Lojo {he/him} <53077361+lomar92@users.noreply.github.com>
2024-05-07 18:13:57 +0200

    Update scope.tf
    adding user admin with role & grants, so it matches with the user that is created on the target instance.

--- scope.tf
+++ scope.tf
@@ -19,3 +19,39 @@ resource "boundary_scope" "project" {
   auto_create_admin_role   = true
   auto_create_default_role = true
 }
+
+# Create a User with sudo privileges on Boundary when accessing target
+resource "boundary_auth_method" "password" {
+  scope_id = boundary_scope.org.id
+  type     = "password"
+}
+
+resource "boundary_account_password" "admin" {
+  auth_method_id = boundary_auth_method.password.id
+  login_name     = "admin"
+  password       = "password"
+}
+
+resource "boundary_user" "admin" {
+  name        = "admin"
+  description = "Admin's user resource"
+  account_ids = [boundary_account_password.admin.id]
+  scope_id    = boundary_scope.org.id
+}
+
+
+resource "boundary_role" "admin" {
+  name          = "Admin"
+  description   = "An Admin role"
+  principal_ids = [boundary_user.admin.id]
+  grant_strings = ["ids=*;type=*;actions=*"]
+  scope_id      = boundary_scope.org.id
+}
+
+resource "boundary_role" "ops-admin" {
+  name          = "Admin"
+  description   = "An Admin role in ops project"
+  principal_ids = [boundary_user.admin.id]
+  grant_strings = ["ids=*;type=*;actions=*"]
+  scope_id      = boundary_scope.project.id
+}
